50c6b108b20532716b832aeb52d4131d49c92548
Adrian <adferrer0611@gmail.com>
2024-08-10 18:00:09 -0400

    Ordering microservice started

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+           // services.AddCarter();
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            //app.UseCarter();
+
+            return app;
+        }
+    }
+}
